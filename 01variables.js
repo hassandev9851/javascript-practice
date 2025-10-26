88d2c2a0e6df7d9d4ae451c3edd41ddb8542eb73
hassancoding123 <jani.ki.jan307@gmail.com>
2025-10-26 12:53:01 -0700

    new file

--- 01variables.js
+++ 01variables.js
@@ -2,3 +2,20 @@ let a = 6;
 let b = 5;
 
 console.log("mul =", a * b);
+
+var name = "Hassan Ali";
+let email = "ali@.com";
+const country = "Pakistan";
+
+let ali;
+
+/*
+console.log(name);
+console.log(email);
+console.log(country);
+console.log(ali);
+*/
+
+
+
+console.table([name, email, country])
---
+++ vare.js
@@ -0,0 +1,23 @@
+var name = "HASSASN";
+let email = "ali@.com";
+const country = "PaKiStAn";
+
+
+
+name = "Hassan";
+let ali;
+
+
+
+/*
+console.log(name);
+console.log(email);
+console.log(country);
+console.log(ali);
+*/
+
+
+console.table([name, email, country, ali])
+
+
+
