467f637e5bc3a9f8d978095adc0d8f4a2498321e
hassancoding123 <jani.ki.jan307@gmail.com>
2025-10-26 12:56:59 -0700

    del

--- 01variables.js
+++ 01variables.js
@@ -3,19 +3,4 @@ let b = 5;
 
 console.log("mul =", a * b);
 
-var name = "Hassan Ali";
-let email = "ali@.com";
-const country = "Pakistan";
 
-let ali;
-
-/*
-console.log(name);
-console.log(email);
-console.log(country);
-console.log(ali);
-*/
-
-
-
-console.table([name, email, country])
